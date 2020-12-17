28fe0eb4af34b73be011bc6b6de6cc1dae64f6ba
Oakarmin Iron <62949666+OakarminIron@users.noreply.github.com>
2020-12-17 17:04:09 +0630

    ErrorDragrable
    Draggable

--- lib/Widgets/products_grid.dart
+++ lib/Widgets/products_grid.dart
@@ -4,25 +4,34 @@ import '../Model/dummyproducts.dart';
 import '../Widgets/productitem.dart';
 
 class ProductsGrid extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Dummyproducts>(context);
     final products = productsData.items;
-    return GridView.builder(
-      padding: const EdgeInsets.all(10.0),
-      itemCount: products.length,
-      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
-        value: products[i],
-        child: ProductItem(),
-      ),
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-      ),
-    );
+    return Column (children: <Widget>[
+    GridView.builder(
+      shrinkWrap: true,
+      primary: true,
+    padding: const EdgeInsets.all(10.0),
+    itemCount: products.length,
+    itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+    value: products[i],
+    child: Draggable(
+        feedback:ProductItem(),
+        child:ProductItem(),
+        childWhenDragging: ProductItem(),
+        onDragEnd: (details) => print(details.offset))
+    ),
+    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+    crossAxisCount: 2,
+    childAspectRatio: 3 / 2,
+    crossAxisSpacing: 10,
+    mainAxisSpacing: 10,
+    ),
+    ),
+
+    ],);
+
+
   }
 }
